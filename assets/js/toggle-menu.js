bb513852f7aa4c49b578819917b046a1f4d7aa6e
Anartz Kalegain <100081287+AnartzKalegain@users.noreply.github.com>
2025-08-16 18:20:38 +0200

    corregido menu desplegable y añadido sombras con scroll

--- assets/js/toggle-menu.js
+++ assets/js/toggle-menu.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.querySelector(".menu-toggle");
   const menu = document.querySelector(".menu-mobile");
+  const navbar = document.querySelector(".navbar");
 
   toggle.addEventListener("click", () => {
     menu.classList.toggle("show");
+  });
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add("shadow");
+    } else {
+      navbar.classList.remove("shadow");
+    }
   });
 });
